6204d070b44264219c78e0c36a62003d182e335e
bamoah_30 <bamoah847@gmail.com>
2025-10-14 16:55:27 +0000

    Delete random_game.py

--- random_game.py
+++
@@ -1,8 +0,0 @@
-import random
-
-your_num = int(input("Enter your number: "))
-comp_num = random.randint(1, 10)
-if your_num == comp_num:
-    print(f"You win! The winning number is {comp_num}")
-else:
-    print(f"You lose! Your number is {your_num}, the computer  number is {comp_num}")
