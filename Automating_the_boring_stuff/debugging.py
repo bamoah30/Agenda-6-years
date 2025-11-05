a19b683387bfe0409289cc946b28380ff19bd45b
Bernard Amoah <bamoah847@gmail.com>
2025-11-05 08:46:54 +0000

    Week 5\ Wednesday

---
+++ Automating_the_boring_stuff/debugging.py
@@ -0,0 +1,22 @@
+#November 5, 2025
+
+'''Traceback:
+A treasure of trove error information produced by pythin whe it encounters an error.
+Traceback includes:
+1.Error message
+2.The line number of the line that caused the error
+3.The sequence of the function calls that led to the error.
+NB: The sequence of calls is known as the call stack
+'''
+
+
+'''Assertion:
+An assertion is a sanity check to make sure the code isn't doing something obviously wrong.
+These sanity checks are performed by assert statements.
+If the sanity checks fails, then an AssertionError is raised.
+An assert statement consists of the following:
+1.The assert keyword
+2.A condition that evaluaters to True or False
+3.A comma
+4. A string to display when the condition is false.
+'''
