a851a8b7cfd251881f5028ba084b4dd3016b2c0a
Bernard Amoah <bamoah847@gmail.com>
2025-11-19 05:59:20 +0000

    Week 7 , day 2

---
+++ Automating_the_boring_stuff/pdf_worddoc.py
@@ -0,0 +1,12 @@
+#November 19, 2025
+'''Pdf and Word Documents:
+Pdf and word documents are binary files that require special libraries to read and write.
+A binary file contains data encoded in binary (0s and 1s), structured for machine interpretation.
+Modules for this section:
+-PyPDF2 - for reading and writing PDF files
+-python-docx - for reading and writing Word (.docx) files
+
+'''
+# PDF
+'''.pdf: portable document format, a file format developed by Adobe to present documents consistently across various devices and platforms.
+'''
