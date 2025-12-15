c56c20a4aeac5b3c0edf11a5dcebe20dee9fbf69
Bernard Amoah <bamoah847@gmail.com>
2025-12-15 19:33:06 +0000

    Concluding lessons on the time module

--- Automating_the_boring_stuff/time_datetime.py
+++ Automating_the_boring_stuff/time_datetime.py
@@ -134,3 +134,32 @@ while datetime.datetime.now() < Christmas2025:
  print('Here we go')
  time.sleep(1)
 
+
+ #December 15, 2025
+ '''Review of Python’s Time Functions:
+•	 A Unix epoch timestamp (used by the time module) is a float or integer 
+value of the number of seconds since 12 am on January 1, 1970, UTC.
+
+•	 A datetime object (of the datetime module) has integers stored in the 
+attributes year, month, day, hour, minute, and second.
+
+•	 The time.time() function returns an epoch timestamp float value of the 
+current moment.
+
+•	 The time.sleep(seconds) function stops the program for the amount of seconds specified by the seconds argument.
+
+•	 The datetime.datetime(year, month, day, hour, minute, second) function 
+returns a datetime object of the moment specified by the arguments. If 
+hour, minute, or second arguments are not provided, they default to 0.
+
+•	 The datetime.datetime.now() function returns a datetime object of the current moment.
+
+•	 The datetime.datetime.fromtimestamp(epoch) function returns a datetime
+object of the moment represented by the epoch timestamp argument.
+
+•	 The total_seconds() method for timedelta objects returns the number of 
+seconds the timedelta object represents.
+
+'''
+
+
